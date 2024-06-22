f41ac58623433468776dd4386dc8ff07c15ceb5a
Karen Dantas <karennobre74@gmail.com>
2024-06-22 12:26:17 -0300

    docs: configure nativewind

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -3,7 +3,7 @@ import { SafeAreaView, Text } from 'react-native';
 export function Home(){
     return (
         <SafeAreaView>
-            <Text> Oi </Text>
+            <Text className = 'text-pink-300'> Oi </Text>
         </SafeAreaView>
     )
 }
