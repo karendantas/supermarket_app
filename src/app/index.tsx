16d5ea79bf76c2c08f5e74ff3b4dddbaccbe46fd
Karen Dantas <karennobre74@gmail.com>
2024-06-22 12:02:11 -0300

    feat: first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Home } from "@/screens/home";
+
+export default function App (){
+    return (
+        <>
+            <Home/>
+        </>
+    )
+}
---
+++ src/screens/home.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from 'react-native';
+
+export function Home(){
+    return (
+        <SafeAreaView>
+            <Text> Oi </Text>
+        </SafeAreaView>
+    )
+}
