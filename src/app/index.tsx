f8661e23721b8882b12e006dd5a9646ed525530e
Karen Dantas <karennobre74@gmail.com>
2024-07-06 17:38:00 -0300

    refactor: remove category-button component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,27 +1,10 @@
-import { CategoryButton } from '@/components/category-button';
-import { useState } from 'react';
 import { FlatList, SafeAreaView, Text } from 'react-native';
 
 export function Home(){
-    const categories = ["Frutas", "Limpeza", "Alimentos"]
-    const [categorySelected, setCategorySelected] = useState(categories[0])
-
-    function changeSelectedCategory( categorySelected: string ){
-        setCategorySelected(categorySelected)
-    }
+    
     return (
         <SafeAreaView>
-            <FlatList 
-                data = {categories}
-                keyExtractor={(item) => item}
-                renderItem={ ({item}) => (
-                    <CategoryButton title = {item}/>
-                )}
-                horizontal
-                showsHorizontalScrollIndicator = {false}
-                className='pt-8 px-4'
-                contentContainerStyle = {{gap: 12}}
-            />
+        
             
         </SafeAreaView>
     )
